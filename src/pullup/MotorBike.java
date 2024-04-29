0ab59369dad27e679858b5b045e69a2c5a6a0b5a
jmar483m <jmar483m#g.educaand.es>
2024-04-29 10:44:55 +0200

    refactorizacion realizada

--- src/pullup/MotorBike.java
+++ src/pullup/MotorBike.java
@@ -1,9 +1,6 @@
 package pullup;
 
 public class MotorBike extends Vehicle {
-	private String matricula;
 	private String casco;
 
-	public void start() {
-	}
 }
--- src/pullup/Vehicle.java
+++ src/pullup/Vehicle.java
@@ -3,4 +3,8 @@ package pullup;
 
 public class Vehicle {
 	protected String nombre;
+    protected String matricula;
+
+    public void start() {
+    }
 }
